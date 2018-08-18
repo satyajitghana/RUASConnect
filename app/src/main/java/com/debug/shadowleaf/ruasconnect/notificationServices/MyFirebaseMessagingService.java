4d09459e75090bd5c0aa487237f5d80a9e37481b
Satyajit Ghana <satyajitghana7@gmail.com>
2018-08-18 16:00:47 +0530

    added Notification

--- app/src/main/java/com/debug/shadowleaf/ruasconnect/notificationServices/MyFirebaseMessagingService.java
+++ app/src/main/java/com/debug/shadowleaf/ruasconnect/notificationServices/MyFirebaseMessagingService.java
@@ -1,8 +1,22 @@
 package com.debug.shadowleaf.ruasconnect.notificationServices;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.debug.shadowleaf.ruasconnect.ChatActivity;
+import com.debug.shadowleaf.ruasconnect.R;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
@@ -16,6 +30,12 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     public static final String TAG = "MyFirebaseMsgService";
@@ -27,6 +47,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            String urlImage = remoteMessage.getData().get("image");
+            String body = remoteMessage.getData().get("body");
+            String title = remoteMessage.getData().get("title");
+
+            Log.d(TAG, urlImage);
+
+            InputStream in;
+            Bitmap myBitmap = null;
+            try {
+                URL url = new URL(urlImage);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                in = connection.getInputStream();
+                myBitmap = BitmapFactory.decodeStream(in);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            sendNotification(body, myBitmap);
 
             if (/* Check if data needs to be processed by long running job */ true) {
                 // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
@@ -77,34 +120,55 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
     }
 
-//    private void sendNotification(String messageBody) {
-//        Intent intent = new Intent(this, NotificationActivity.class);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-//                PendingIntent.FLAG_ONE_SHOT);
+    private void sendNotification(String messageBody, Bitmap myBitmap) {
+        Intent intent = new Intent(this, ChatActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle()
+                .bigPicture(myBitmap)
+                .setSummaryText("Summary Text");
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_cloud_06)
+                        .setContentTitle("FCM Message")
+                        .setContentText(messageBody)
+                        .setLargeIcon(myBitmap)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent)
+                        .setStyle(style);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        Intent intent = new Intent(this, ChatActivity.class);
+//        intent.putExtra("key", "value");
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 100, intent, PendingIntent.FLAG_ONE_SHOT);
 //
-//        String channelId = getString(R.string.default_notification_channel_id);
-//        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//        NotificationCompat.Builder notificationBuilder =
-//                new NotificationCompat.Builder(this, channelId)
-//                        .setSmallIcon(R.drawable.ic_stat_ic_notification)
-//                        .setContentTitle("FCM Message")
-//                        .setContentText(messageBody)
-//                        .setAutoCancel(true)
-//                        .setSound(defaultSoundUri)
-//                        .setContentIntent(pendingIntent);
-//
-//        NotificationManager notificationManager =
-//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//        // Since android Oreo notification channel is needed.
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            NotificationChannel channel = new NotificationChannel(channelId,
-//                    "Channel human readable title",
-//                    NotificationManager.IMPORTANCE_DEFAULT);
-//            notificationManager.createNotificationChannel(channel);
-//        }
-//
-//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
-//    }
+//        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        Notification notif = new Notification.Builder(this)
+//                .setContentIntent(pendingIntent)
+//                .setContentTitle("FCM Message")
+//                .setContentText(messageBody)
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setLargeIcon(myBitmap)
+//                .setStyle(new Notification.BigPictureStyle().bigPicture(myBitmap))
+//                .build();
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        //notificationManager.notify(0, notif);
+    }
 }
